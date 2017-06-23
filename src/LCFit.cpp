9b566b19203abb704e4833520b7577a5651a1831
Rob Firth <ref1g12@soton.ac.uk>
2017-06-23 15:53:12 +0100

    commented out the fading early emission from SN2011fu. Tried the Firth17Complex model but it couldn't recreate the behaviourmake

--- src/LCFit.cpp
+++ src/LCFit.cpp
@@ -25,6 +25,8 @@
 #include "core/utils.hpp"
 #include "core/SN.hpp"
 #include "models/Karpenka12.hpp"
+#include "Karpenka12Afterglow.hpp"
+#include "models/Firth17Complex.hpp"
 #include "solvers/MNest.hpp"
 
 
@@ -144,11 +146,22 @@ void fitLC(std::shared_ptr<Workspace> w) {
         // Loop though each filter
         for (auto lc : sn.second.lc_) {
             // Initialise the model
-            std::shared_ptr<Karpenka12> karpenka12(new Karpenka12);
-            karpenka12->x_ = vmath::sub<double>(lc.second.mjd_, lc.second.mjdMin_);
-            karpenka12->y_ = vmath::div<double>(lc.second.flux_, lc.second.normalization_);
-            karpenka12->sigma_ = vmath::div<double>(lc.second.fluxErr_, lc.second.normalization_);
-            std::shared_ptr<Model> model = std::dynamic_pointer_cast<Model>(karpenka12);
+            // std::shared_ptr<Karpenka12> karpenka12(new Karpenka12);
+            // karpenka12->x_ = vmath::sub<double>(lc.second.mjd_, lc.second.mjdMin_);
+            // karpenka12->y_ = vmath::div<double>(lc.second.flux_, lc.second.normalization_);
+            // karpenka12->sigma_ = vmath::div<double>(lc.second.fluxErr_, lc.second.normalization_);
+            // std::shared_ptr<Model> model = std::dynamic_pointer_cast<Model>(karpenka12);
+            std::shared_ptr<Karpenka12Afterglow> karpenka12afterglow(new Karpenka12Afterglow);
+            karpenka12afterglow->x_ = vmath::sub<double>(lc.second.mjd_, lc.second.mjdMin_);
+            karpenka12afterglow->y_ = vmath::div<double>(lc.second.flux_, lc.second.normalization_);
+            karpenka12afterglow->sigma_ = vmath::div<double>(lc.second.fluxErr_, lc.second.normalization_);
+            std::shared_ptr<Model> model = std::dynamic_pointer_cast<Model>(karpenka12afterglow);
+
+            // std::shared_ptr<Firth17Complex> firth17complex(new Firth17Complex);
+            // firth17complex->x_ = vmath::sub<double>(lc.second.mjd_, lc.second.mjdMin_);
+            // firth17complex->y_ = vmath::div<double>(lc.second.flux_, lc.second.normalization_);
+            // firth17complex->sigma_ = vmath::div<double>(lc.second.fluxErr_, lc.second.normalization_);
+            // std::shared_ptr<Model> model = std::dynamic_pointer_cast<Model>(firth17complex);
 
             // Initialise solver
             MNest solver(model);
