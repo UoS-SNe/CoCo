83aaee61dad094c93e022fb4f8fc9e9c1f9927fc
Darren J. White <djwhiteastro@users.noreply.github.com>
2018-06-27 16:03:03 +0100

    Updated CoCoJobSplit.sh
    Updated to match deliverable, tidied up code/comments

--- GridPP/CoCoJobSplit.sh
+++ GridPP/CoCoJobSplit.sh
@@ -1,42 +1,38 @@
 #!/usr/bin/env bash
 start=`date +%s`
-#Echo all commands
+
+# Echo all commands
 set -o xtrace
 
-#Parameters
+# Run parameters set via Ganga
 EUPS=$1
 SPECTRA=$2
 INFILE=$3
 NLCS=$4
 
-echo "Cloning CoCo from github"
+# Retrieving code from GitHub - git not installed on some nodes, so use wget
+echo "Retrieving CoCo from GitHub"
 wget https://github.com/UoS-SNe/CoCo/archive/gridPP.zip
 unzip gridPP.zip 
 mv CoCo-gridPP CoCo
 
-endclone=`date +%s`
-echo "$((endclone-start)) seconds to clone from github" >> $(basename $INFILE.log.dat)
-
+# Unpack tarballs containing Spectra and EUPS directories
 echo "Unpacking EUPS and Spectra tarballs into CoCo directory"
 tar -xzf $(basename $EUPS) --directory CoCo 2>&1
 tar -xzf $(basename $SPECTRA) --directory CoCo 2>&1
 
-enduntar=`date +%s`
-echo "$((enduntar-endclone)) seconds to untar" >> $(basename $INFILE.log.dat)
-
+# Copy files to correct locations in CoCo directory 
 cp $INFILE CoCo
 cd CoCo 
-echo "Switching to gridPP branch"
-git checkout gridPP 
 cp GridPP/CoCosim2.py ./
 cp GridPP/sim_functions.py ./
 
+# Use EUPS to set up prerequisites
 echo "Setting up EUPS"
 source /cvmfs/lsst.opensciencegrid.org/fnal/products/eups/bin/setups.sh
 MY_EUPS=$PWD/eups
 export EUPS_PATH=$MY_EUPS:/cvmfs/lsst.opensciencegrid.org/fnal/products/
 echo $EUPS_PATH
-
 echo "Setup required modules"
 echo "gcc v4_9_2"; setup gcc v4_9_2
 echo "gsl"; setup gsl
@@ -46,21 +42,13 @@ echo "minuit2"; setup minuit2
 echo "scipy v0_14_0"; setup scipy v0_14_0
 echo "python v2_7_8"; setup python v2_7_8
 
-endeups=`date +%s`
-echo "$((endeups-enduntar)) seconds to set up EUPS" >> ../$(basename $INFILE.log.dat)
-
+# Make and build CoCo libraries
 echo "Building CoCo"
 make
 python setup.py build_ext --inplace
 
-endbuild=`date +%s`
-echo "$((endbuild-endeups)) seconds to build pyCoCo" >> ../$(basename $INFILE.log.dat)
-
+# Run simulation script for input file
 echo "Running on" `date`
 echo "Running CoCo: python CoCoSim2.py "
 python CoCosim2.py $(basename $INFILE) $NLCS
 echo "DONE"
-
-endsim=`date +%s`
-echo "$((endsim-endbuild)) seconds to run simulations" >> ../$(basename $INFILE.log.dat)
-echo "$((endsim-start)) seconds to run whole script" >> ../$(basename $INFILE.log.dat)
